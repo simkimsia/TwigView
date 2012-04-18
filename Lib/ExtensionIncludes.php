3fa783e7a5eb22375d7016a6f2a01fb8f3cc5399
kimsia <kimcity@gmail.com>
2012-04-18 15:00:34 +0800

    Add a file to include all the project specific extensions

---
+++ Lib/ExtensionIncludes.php
@@ -0,0 +1,10 @@
+<?php
+
+$twigPath = CakePlugin::path('TwigView');
+
+// overwrite Twig_Environment class with project specific Twig_Environment.php
+//require_once($twigPath . 'Lib' . DS . 'Project_Twig_Environment.php');
+
+// custom project Twig_Extension class
+//require_once($twigPath . 'Lib' . DS . 'Project_Twig_Extension.php');
+
